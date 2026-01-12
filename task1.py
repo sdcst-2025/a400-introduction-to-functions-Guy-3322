7b9376733ea8f17963c32f2b22c2d978d6de81f6
Guy-3322 <rpreiswerck@gmail.com>
2026-01-12 08:58:52 -0800

    done

--- task1.py
+++ task1.py
@@ -9,7 +9,13 @@ Execute the function of the letter they use.
 """
 
 def A():
-
+ print("Hello")
 def B():
-
+ print("How are you")
 def C():
+ print("Hi")
+
+
+A()
+B()
+C()
--- task2.py
+++ task2.py
@@ -10,3 +10,27 @@ plays the game
 This will be silimar to something you have already done, but in this task you 
 are breaking the code up into different sections to make each a function.
 """
+import random
+
+def Title():
+    print("Guess a number from 1 to 100")
+    print("If it's right you win if not guess again")
+    print("----------------------------------------")
+def Game():
+    num = random.randint(1,100)
+    win = False
+    while win == False:
+     guess= int(input("Guess a number:"))
+     if guess == num:
+         print("Yay you win!!")
+         win = True
+     else:
+         print("Wrong try again")
+         continue
+
+
+Title()   
+Game()
+
+
+
